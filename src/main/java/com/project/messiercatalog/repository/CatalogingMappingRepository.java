19ebcc524e4a8d23d0d1e51cf6c7bb602a015f1c
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-07 05:26:48 -0300

    Criando repository de acesso a entidade CatalogingMapping
    Criando repository de acesso a entidade CatalogingMapping

---
+++ src/main/java/com/project/messiercatalog/repository/CatalogingMappingRepository.java
@@ -0,0 +1,9 @@
+package com.project.messiercatalog.repository;
+
+import com.project.messiercatalog.model.CatalogingMapping;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatalogingMappingRepository extends JpaRepository<CatalogingMapping, Long>{
+
+}
