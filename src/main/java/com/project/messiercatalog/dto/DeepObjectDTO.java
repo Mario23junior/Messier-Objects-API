ea9d71e0340fde254400d60521d903512a2c1784
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-07 05:38:58 -0300

    Criando a entidade DeepObjectDTO DTO.

---
+++ src/main/java/com/project/messiercatalog/dto/DeepObjectDTO.java
@@ -0,0 +1,54 @@
+package com.project.messiercatalog.dto;
+
+public class DeepObjectDTO {
+  
+	private Long id;
+	private String image;
+	private String constelacao;
+	private String nome;
+	private Double magnitude;
+
+	public DeepObjectDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getConstelacao() {
+		return constelacao;
+	}
+
+	public void setConstelacao(String constelacao) {
+		this.constelacao = constelacao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getMagnitude() {
+		return magnitude;
+	}
+
+	public void setMagnitude(Double magnitude) {
+		this.magnitude = magnitude;
+	}
+}
