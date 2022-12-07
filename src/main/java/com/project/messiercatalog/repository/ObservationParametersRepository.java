5969a4d1be99d2866909331dee1b49cb20615f4c
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-07 05:27:16 -0300

    Criando repository de acesso a entidade ObservationParameters

---
+++ src/main/java/com/project/messiercatalog/repository/ObservationParametersRepository.java
@@ -0,0 +1,9 @@
+package com.project.messiercatalog.repository;
+
+import com.project.messiercatalog.model.ObservationParameters;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ObservationParametersRepository extends JpaRepository<ObservationParameters, Long>{
+
+}
