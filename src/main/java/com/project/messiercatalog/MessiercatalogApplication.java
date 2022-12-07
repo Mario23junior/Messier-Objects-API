02bd4b21c34e1f9ee903bb3172730fd1e2c49834
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-07 03:19:39 -0300

    Criando catalago messier

---
+++ src/main/java/com/project/messiercatalog/MessiercatalogApplication.java
@@ -0,0 +1,13 @@
+package com.project.messiercatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessiercatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessiercatalogApplication.class, args);
+	}
+
+}
