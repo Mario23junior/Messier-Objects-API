6e5cc66909fb224b993910db46f778e8c4f732a8
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-07 05:27:02 -0300

    Criando repository de acesso a entidade DeepObject
    Criando repository de acesso a entidade DeepObject

---
+++ src/main/java/com/project/messiercatalog/repository/DeepObjectRepository.java
@@ -0,0 +1,9 @@
+package com.project.messiercatalog.repository;
+
+import com.project.messiercatalog.model.DeepObject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeepObjectRepository extends JpaRepository<DeepObject, Long>{
+
+}
