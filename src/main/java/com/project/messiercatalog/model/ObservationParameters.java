f709f8755f2f821c90d985559bad8bc489b0299e
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-07 05:13:58 -0300

    Criando base de dados da entidade ObservationParameters.

---
+++ src/main/java/com/project/messiercatalog/model/ObservationParameters.java
@@ -0,0 +1,55 @@
+package com.project.messiercatalog.model;
+
+public class ObservationParameters {
+
+	private Long id;
+	private String tipo;
+	private String assecaoReta;
+	private Double idade;
+	private Double DistanciaTerra;
+
+	public ObservationParameters() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getAssecaoReta() {
+		return assecaoReta;
+	}
+
+	public void setAssecaoReta(String assecaoReta) {
+		this.assecaoReta = assecaoReta;
+	}
+
+	public Double getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Double idade) {
+		this.idade = idade;
+	}
+
+	public Double getDistanciaTerra() {
+		return DistanciaTerra;
+	}
+
+	public void setDistanciaTerra(Double distanciaTerra) {
+		DistanciaTerra = distanciaTerra;
+	}
+
+}
