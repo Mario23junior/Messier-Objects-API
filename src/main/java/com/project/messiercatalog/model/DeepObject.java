a5b347f8612e2eb1965a68c81540563d5871900d
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-07 05:13:15 -0300

    Criando base de dados da entidade DeepObject.
    Criando base de dados da entidade DeepObject.

---
+++ src/main/java/com/project/messiercatalog/model/DeepObject.java
@@ -0,0 +1,63 @@
+package com.project.messiercatalog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class DeepObject {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String image;
+	private String constelacao;
+	private String nome;
+	private Double magnitude;
+
+	public DeepObject() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getConstelacao() {
+		return constelacao;
+	}
+
+	public void setConstelacao(String constelacao) {
+		this.constelacao = constelacao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getMagnitude() {
+		return magnitude;
+	}
+
+	public void setMagnitude(Double magnitude) {
+		this.magnitude = magnitude;
+	}
+
+}
