a0474f2557b1a9829279d624f1b3ef01d94db07f
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-10 02:25:57 -0300

    Obter parametros por id de identificação.

--- src/main/java/com/project/messiercatalog/controller/ObservationParametersController.java
+++ src/main/java/com/project/messiercatalog/controller/ObservationParametersController.java
@@ -4,6 +4,8 @@ import com.project.messiercatalog.dto.ObservationParametersDTO;
 import com.project.messiercatalog.service.ObservationParametersService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,11 @@ public class ObservationParametersController {
 	public ResponseEntity<ObservationParametersDTO> save(
 			@RequestBody ObservationParametersDTO obserParaDto) {
 		return service.save(obserParaDto);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<ObservationParametersDTO> list(@PathVariable Long id) {
+		return service.listId(id);
 	}
 	
  }
--- src/main/java/com/project/messiercatalog/service/ObservationParametersService.java
+++ src/main/java/com/project/messiercatalog/service/ObservationParametersService.java
@@ -1,5 +1,8 @@
 package com.project.messiercatalog.service;
 
+import java.util.Optional;
+
+import com.project.messiercatalog.dto.DeepObjectDTO;
 import com.project.messiercatalog.dto.ObservationParametersDTO;
 import com.project.messiercatalog.exceptions.HandlingErrorReturnMessage;
 import com.project.messiercatalog.model.ObservationParameters;
@@ -12,26 +15,35 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ObservationParametersService {
- 
+
 	private ModelMapper mapper;
 	private ObservationParametersRepository repository;
-	
-	public ObservationParametersService(
-			ObservationParametersRepository repository,
-			ModelMapper mapper) {
+
+	public ObservationParametersService(ObservationParametersRepository repository, ModelMapper mapper) {
 		this.mapper = mapper;
 		this.repository = repository;
 	}
-	
-	public ResponseEntity<ObservationParametersDTO>save(ObservationParametersDTO obserVationDto){
+
+	public ResponseEntity<ObservationParametersDTO> save(ObservationParametersDTO obserVationDto) {
 		try {
 			ObservationParameters convert = mapper.map(obserVationDto, ObservationParameters.class);
 			ObservationParameters convertbase = repository.save(convert);
 			ResponseEntity<ObservationParametersDTO> response = ResponseEntity.status(HttpStatus.OK)
-					.body(mapper.map(convertbase,  ObservationParametersDTO.class));
+					.body(mapper.map(convertbase, ObservationParametersDTO.class));
 			return response;
 		} catch (Exception e) {
 			throw new HandlingErrorReturnMessage("Erro ao salvar parametro de observação.");
 		}
- 	}
+	}
+
+	public ResponseEntity<ObservationParametersDTO> listId(Long id) {
+		Optional<ObservationParameters> findid = repository.findById(id);
+		if (findid.isPresent()) {
+			ObservationParametersDTO convert = mapper.map(findid.get(), ObservationParametersDTO.class);
+			return ResponseEntity.ok(convert);
+		} else {
+			throw new HandlingErrorReturnMessage(
+					"Erro ao buscar objeto de id : " + id + ", Por favor tente mais tarde");
+		}
+	}
 }
