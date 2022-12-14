0077ecd1a1f2368c17ed7a4414d550d52a565f8b
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-12-14 08:49:02 -0300

    Configurando cors de segunrança para acesso a aplicações externas.

--- src/main/java/com/project/messiercatalog/controller/DeepObjectCotroller.java
+++ src/main/java/com/project/messiercatalog/controller/DeepObjectCotroller.java
@@ -6,6 +6,7 @@ import com.project.messiercatalog.dto.DeepObjectDTO;
 import com.project.messiercatalog.service.DeepObjectService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/project/catalog/")
 public class DeepObjectCotroller {
 
